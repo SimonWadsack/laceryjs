34257fd812726d93f816c8ec9036f827ea43910d
Simon Wadsack <dev.simon@wadsack.net>
2025-05-07 20:51:36 +0200

    SelectElements support for preview images

--- src/elements/numberSelectElement.ts
+++ src/elements/numberSelectElement.ts
@@ -5,9 +5,13 @@ import { SlSelect, SlOption } from '@shoelace-style/shoelace';
  * Configuration options for a `NumberSelectElement`.
  * 
  * @property help - Optional help text to display with the select element.
+ * @property previews - Optional array of preview image srcs to display.
+ * @property previewSize - Optional size of the preview images in pixel.
  */
 interface NumberSelectOptions {
     help?: string;
+    previews? : string[];
+    previewSize?: number;
 }
 
 /**
@@ -30,17 +34,33 @@ export class NumberSelectElement extends LaceElement{
      * @param options - Optional configuration for the number select element.
      */
     constructor(label: string, obj: any, key: string, selectOptions: { [key: number]: string } , options: NumberSelectOptions = {}) {
-        const {help = ""} = options;
+        const {help = "", previews = [], previewSize = 40} = options;
         if(!selectOptions[obj[key]]) selectOptions[obj[key]] = "Original";
         const select: SlSelect = document.createElement('sl-select');
         select.label = label;
         select.helpText = help;
         select.placeholder = '-';
 
+        var index = 0;
+        const showPreviews = previews.length > 0 && previews.length == Object.keys(selectOptions).length;
+        if(!showPreviews) console.warn("Previews are not shown because the number of previews does not match the number of options.");
+
         for(const key in selectOptions){
             const option: SlOption = document.createElement('sl-option');
             option.value = key.toString();
-            option.textContent = selectOptions[key];
+            option.appendChild(document.createTextNode(selectOptions[key]));
+
+            // Add preview images if provided
+            if(showPreviews) {
+                const img = document.createElement('img');
+                img.src = previews[index];
+                img.style.width = `${previewSize}px`;
+                img.style.height = `${previewSize}px`;
+                img.slot = "prefix";
+                option.appendChild(img);
+                index++;
+            }
+
             select.appendChild(option);
         }
 
